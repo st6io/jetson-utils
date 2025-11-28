96e2d6faa420baa9e78dfb86af73d14b600e2d59
svetlin <svetlin@st6.io>
2025-11-28 15:05:26 +0200

    Improve the logs

--- codec/gstBufferManager.cpp
+++ codec/gstBufferManager.cpp
@@ -298,7 +298,7 @@ bool gstBufferManager::Enqueue( GstBuffer* gstBuffer, GstCaps* gstCaps )
 // Dequeue
 int gstBufferManager::Dequeue( void** output, imageFormat format, uint64_t timeout, cudaStream_t stream )
 {
-	LogInfo(LOG_AMERU "Dequeue");
+	LogInfo(LOG_AMERU "Dequeue\n");
 	// wait until a new frame is recieved
 	if( !mWaitEvent.Wait(timeout) )
 		return 0;
--- codec/gstDecoder.cpp
+++ codec/gstDecoder.cpp
@@ -783,7 +783,7 @@ GstFlowReturn gstDecoder::onPreroll( _GstAppSink* sink, void* user_data )
 // onBuffer
 GstFlowReturn gstDecoder::onBuffer( _GstAppSink* sink, void* user_data )
 {
-	LogInfo(LOG_AMERU "onBuffer");
+	LogInfo(LOG_AMERU "onBuffer\n");
 	
 	if( !user_data )
 		return GST_FLOW_OK;
